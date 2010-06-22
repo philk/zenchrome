2ad13f83860278375df47e657fff0aac5d21fe81
Phil Kates <me@philkates.com>
2010-06-22 15:26:56 -0700

    Moved functions into support.js

--- js/support.js
+++ js/support.js
@@ -1,16 +1,35 @@
-function getNumberOfTickets(rule) {
-  var tickets = null;
-  $.ajax({
-    url: "https://leapfile.zendesk.com/rules/"+rule+".json",
-    async: false,
-    dataType: 'json',
-    success: function(json){
-      tickets = json.length;
-    }
-  });
-  return tickets;
-  // $.getJSON("https://leapfile.zendesk.com/rules/"+list.rule+".json", list, function(json){
-    // $('#working').append("Working Tickets ("+json.length+")");
-    // $('#main').after('<li><a href="#" id="'+list.name+'">'+list.display+' ('+json.length+')');
-  // });
+// Taken from http://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage/2010994#2010994
+Storage.prototype.setObject = function(key, value) {
+    this.setItem(key, JSON.stringify(value));
 };
+
+Storage.prototype.getObject = function(key) {
+    return JSON.parse(this.getItem(key));
+};
+// --
+
+function getNumberOfTickets(view) {
+  return localStorage.getObject(view).length; 
+};
+
+function getTickets(view) {
+    $.getJSON("https://leapfile.zendesk.com/rules/"+view.rule+".json", function(json){
+        localStorage.setObject(view.shortname, json);
+    });
+};
+
+function updateTickets(arr){
+    arr.map(function(a){
+       getTickets(a);
+    });
+    console.log(arr);
+    setTimeout(setTicketNumber(arr[2].shortname), 3000);
+    console.log("Updating");
+};
+
+function setTicketNumber(view) {
+    chrome.browserAction.setBadgeText({
+        text: getNumberOfTickets(view).toString()
+    });
+    console.log("Setting Ticket")
+}
