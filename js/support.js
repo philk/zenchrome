fdde879ea9c99d3620c9870e208c4c7006e36fdd
Phil Kates <me@philkates.com>
2010-07-15 17:53:03 -0700

    Modified initialize table load Row adding/removing working

--- js/support.js
+++ js/support.js
@@ -50,6 +50,12 @@ function delView(viewName){
     localStorage.setObject("views", views);
 };
 
+function delViewById(viewId){
+    var views = localStorage.getObject("views");
+    views.splice(viewId, viewId);
+    localStorage.setObject("views", views);
+};
+
 function getAllTickets(){
     var views = localStorage.getObject("views");
     console.log(views);
@@ -63,4 +69,24 @@ function setTicketNumber(view) {
         text: getNumberOfTickets(view).toString()
     });
     console.log("Setting Ticket")
+};
+
+function addRow(view){
+    console.log("Adding...");
+    var rowId = $("#rules_table tbody tr").length;
+    if (view == undefined) {
+        $("#rules_table tbody").append('<tr id="row'+rowId+'"><td class="dragHandle"></td><td><input type="text" class="input_display" id="inputName'+rowId+'" value=""></td><td><input type="text" class="input_rule" id="inputNumber'+rowId+'" value=""></td><td><a href="#" onClick=\'delRow('+rowId+');\'><img src="img/list-remove.png"/></a></td></tr>');
+    }else{
+        $("#rules_table tbody").append('<tr id="row'+rowId+'"><td class="dragHandle"></td><td><input type="text" class="input_display" id="inputname'+rowId+'" value="'+view.viewName+'"></td><td><input type="text" class="input_rule" id="inputNumber'+rowId+'" value="'+view.viewNumber+'"></td><td><a href="#" onClick=\'delRow('+rowId+');\'><img src="img/list-remove.png"/></a></td></tr>');
+    };
+};
+
+function delRow(row){
+    var viewName = $('#inputName'+row).val();
+    console.log(viewName);
+    if (viewName != undefined) {
+        console.log("View Exists")
+        delViewById(row);
+    };
+    $('#row'+row).remove();
 };
